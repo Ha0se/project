c36c3f3ffa086c097f90ac341acf2426291de3ef
Ha0se <103370660+Ha0se@users.noreply.github.com>
2022-12-02 20:27:15 +0100

    Done some functions mentioned in TODO

--- expenses/forms.py
+++ expenses/forms.py
@@ -1,12 +1,13 @@
 from django import forms
 from .models import Expense
 
-
 class ExpenseSearchForm(forms.ModelForm):
     class Meta:
         model = Expense
-        fields = ('name',)
+        fields = ('name','category','date',)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['name'].required = False
+        self.fields['category'].required = False
+        self.fields['date'].required = False
--- expenses/views.py
+++ expenses/views.py
@@ -1,9 +1,7 @@
 from django.views.generic.list import ListView
-
 from .forms import ExpenseSearchForm
 from .models import Expense, Category
 from .reports import summary_per_category
-
 
 class ExpenseListView(ListView):
     model = Expense
@@ -11,12 +9,18 @@ class ExpenseListView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         queryset = object_list if object_list is not None else self.object_list
-
         form = ExpenseSearchForm(self.request.GET)
+
         if form.is_valid():
-            name = form.cleaned_data.get('name', '').strip()
+            name = form.cleaned_data.get('name').strip()
+            category = form.cleaned_data.get('category')
+            date = form.cleaned_data.get('date')
             if name:
                 queryset = queryset.filter(name__icontains=name)
+            if category:
+                queryset = queryset.filter(category=category)
+            if date:
+                queryset = queryset.filter(date=date)
 
         return super().get_context_data(
             form=form,
@@ -24,7 +28,7 @@ class ExpenseListView(ListView):
             summary_per_category=summary_per_category(queryset),
             **kwargs)
 
+
 class CategoryListView(ListView):
     model = Category
     paginate_by = 5
-
